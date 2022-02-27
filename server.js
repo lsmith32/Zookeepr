88109b4babb29518892782f33ddb203619dd8898
lsmith32 <lmksmith97@gmail.com>
2022-02-26 23:03:22 -0600

    Port updated

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3001;
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -49,6 +50,6 @@ function filterByQuery(query, animalsArray) {
     res.json(results);
   });
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!');
-});
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
